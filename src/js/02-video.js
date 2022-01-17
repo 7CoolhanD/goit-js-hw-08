d83cb00bcabf1f69ff4952bd7923732c7b17b931
CoolhanD <abrawa@gmail.com>
2022-01-17 23:27:57 +0200

    player done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash/throttle';
+
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+player.on(
+  'timeupdate',
+  throttle(function () {
+    player.getCurrentTime().then(sec => {
+      localStorage.setItem('videoplayer-curent-time', JSON.stringify(sec));
+    });
+  }, 1000),
+);
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+const savedStorageTime = JSON.parse(localStorage.getItem('videoplayer-curent-time') || 0);
+player.setCurrentTime(savedStorageTime);
