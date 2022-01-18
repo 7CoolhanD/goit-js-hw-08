819df35ff11f17e19a2c174868e87557682402e4
CoolhanD <abrawa@gmail.com>
2022-01-18 17:48:42 +0200

    form done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('feedback-form input'),
+  message: document.querySelector('feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', formSubmit);
+refs.form.addEventListener('input', throttle(formInput, 500));
+
+populateTextInput();
+
+function formSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function formInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateTextInput() {
+  const userData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (userData && Object.values(userData) !== []) {
+    refs.form.email.value = userData.email;
+    refs.form.message.value = userData.message;
+    formData.email = userData.email;
+    formData.message = userData.message;
+  }
+}
